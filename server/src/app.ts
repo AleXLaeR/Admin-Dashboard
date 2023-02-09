884f2bbeac70c748611218b8581a49a4a8d6178f
Alexander Lazarenko <ferrosful@gmail.com>
2023-02-09 12:54:33 +0200

    Setup basic back-end layout.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,48 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import * as mongoose from 'mongoose';
 
+import {
+  baseRoutes,
+  clientRoutes,
+  salesRoutes,
+  managementRoutes,
+  errorRoutes,
+} from '@routes/index';
+
+/* CONFIGURATION */
+dotenv.config({ path: '.env.local' });
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  return res.send('zchjsdvhjkdfvhjk');
-});
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
-app.listen(5000, () => {
-  console.log('Application running on port 5000!');
-});
+/* ROUTES */
+app.use('/client', clientRoutes);
+app.use('/sales', salesRoutes);
+app.use('/management', managementRoutes);
+app.use('/', baseRoutes, errorRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 8080;
+
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(process.env.MONGODB_URL!)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`There was an error establishing mongodb connection: ${error}`);
+  });
---
+++ server/src/routes/base.routes.ts
@@ -0,0 +1,5 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+export default router;
